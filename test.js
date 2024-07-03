a294096f462765bdf6978b76eb1531dccad5f73c
Sebastijan Kelnerič <sebastijan.kelneric@sebba.dev>
2024-07-03 16:43:51 +0200

    Create minimal reproducable example

---
+++ test.js
@@ -0,0 +1,5 @@
+// This is all it's needed to reproduce the bug
+// System Arch Linux on X11 setup
+// Instructions: Create a new line, hit ctrl + space to open suggestion and start typing
+// E.g.: typing "const" reproduces the bug for me every time - first 2 chars are fine, 3rd one introduces the bug
+// Enjoy
